ab48e229ed432dace04aaa021a257187780123af
Aseem Jain <Aseem.Jain@rackspace.com>
2017-01-29 10:29:50 -0600

    pushing the interactive menu file

---
+++ src/com/premaseem/Car.java
@@ -0,0 +1,24 @@
+package com.premaseem;
+
+/**
+ * Created by asee2278 on 1/25/17.
+ */
+
+public class Car {
+
+
+    String motion = null;
+
+    public static void main(String[] args) {
+
+    }
+
+
+    String move(){
+        return "moving ";
+    }
+
+
+
+
+}
