ca3eb32741adb5d40340af02ac2bb476042d4e57
mohan64670 <mohanrskk25@gmail.com>
2017-02-01 20:48:11 -0500

    sorting algorithm

--- src/com/mohan/Sorting.java
+++ src/com/mohan/Sorting.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public class Sorting {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method 10
 		ArrayList < Integer > arraylist = new ArrayList < Integer > ();
 
 		arraylist.add(100);
