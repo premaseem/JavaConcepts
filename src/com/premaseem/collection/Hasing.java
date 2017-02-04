e50e7d20b3885cf42ab0b728fc1c417c12cd4691
Aseem Jain <Aseem.Jain@rackspace.com>
2017-02-04 12:10:44 -0600

    Optimized hashing further, HW assignment is to format printing with optimized code.

--- src/com/premaseem/collection/Hasing.java
+++ src/com/premaseem/collection/Hasing.java
@@ -39,52 +39,42 @@ public class Hasing {
         List<Book> listgo = new ArrayList<Book>();
 
         Map<Integer,List<Book>> simulatedHashMap = new HashMap<Integer,List<Book>>();
-
+        simulatedHashMap.put(101,listJava);
+        simulatedHashMap.put(102,listJavaScrip);
+        simulatedHashMap.put(103,listgo);
 
 
 
         //Creating Books
         Book b1=new Book(101,"Let us Java","Yashwant Kanetkar",101,8);
-        Book b5=new Book(101,"Bile of java","Yashwant Kanetkar",101,8);
-        Book b8=new Book(101,"From 0 to Hero in Java","Yashwant Kanetkar",101,8);
-        listJava.add(b1);
-        listJava.add(b5);
-        listJava.add(b8);
+        simulatedHashMap.get(b1.hashCode()).add(b1);
 
+        Book b5=new Book(101,"Bile of java","Yashwant Kanetkar",101,8);
+        simulatedHashMap.get(b1.hashCode()).add(b5);
+
+        Book b8=new Book(101,"From 0 to Hero in Java","Yashwant Kanetkar",101,8);
+        simulatedHashMap.get(b1.hashCode()).add(b8);
 
         Book b3=new Book(101,"Let us Javascript","Yashwant Kanetkar",102,8);
-        Book b6=new Book(101,"Bile of javascript","Yashwant Kanetkar",102,8);
-        listJavaScrip.add(b3);
-        listJavaScrip.add(b6);
-        listJavaScrip.add(new Book(420,"Allocate Javascript","Naren",102,12));
+        simulatedHashMap.get(b1.hashCode()).add(b3);
 
+        Book b6=new Book(101,"Bile of javascript","Yashwant Kanetkar",102,8);
+        simulatedHashMap.get(b1.hashCode()).add(b6);
 
         Book b2=new Book(101,"Let us Go","Yashwant Kanetkar",103,8);
+        simulatedHashMap.get(b1.hashCode()).add(b2);
+
         Book b4=new Book(101,"Bile of Golang","Yashwant Kanetkar",103,8);
+        simulatedHashMap.get(b1.hashCode()).add(b4);
+
         Book b7=new Book(101,"From 0 to Hero in javascript","Yashwant Kanetkar",102,8);
-        listgo.add(b2);
-        listgo.add(b4);
-        listgo.add(b7);
+        simulatedHashMap.get(b1.hashCode()).add(b7);
 
-        //Adding Books to map
-//        map.put(1,b1);
-//        map.put(2,b2);
-//        map.put(3,b3);
-//        //Traversing map
-//        for(Map.Entry<Integer, Book> entry:map.entrySet()){
-//            int key=entry.getKey();
-//            Book b=entry.getValue();
-//            System.out.println(key+" Details:");
-//            System.out.println(b.id+" "+b.name+" "+b.author+"  "+b.quantity + " " + b.hashCode());
-//        }
 
-        simulatedHashMap.put(101,listJava);
-        simulatedHashMap.put(102,listJavaScrip);
-        simulatedHashMap.put(103,listgo);
 
         List<Book> returnList= simulatedHashMap.get(101);
         for( Book b : returnList){
-            System.out.println(b.id+" "+b.name+" "+b.author+"  "+b.quantity + " " + b.hashCode());
+            System.out.println(" "+b.name+" "+b.author+"  "+b.quantity + " " + b.hashCode());
         }
 
         returnList= simulatedHashMap.get(102);
@@ -99,6 +89,7 @@ public class Hasing {
         for( Book b : returnList){
             System.out.println(b.id+" "+b.name+" "+b.author+"  "+b.quantity + " " + b.hashCode());
         }
+
 
     }
 }
