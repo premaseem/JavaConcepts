5f799395174abdc387d4e1d20656e6b04dadb81a
Aseem Jain <Aseem.Jain@rackspace.com>
2017-02-07 20:17:07 -0600

    Nested try catch

--- src/com/premaseem/exceptions/TryMe.java
+++ src/com/premaseem/exceptions/TryMe.java
@@ -6,20 +6,25 @@ package com.premaseem.exceptions;
 public class TryMe {
     public static void main(String[] args) {
         Integer data = null;
-        Integer balance = 1;
-        int x = 0;
+        Integer balance = 0;
+        int x = 1;
         try{
             System.out.println("write some to a file");
             data=50/balance;
+//            data = null;
+
+            try{
+                data.toString();
+            }catch(NullPointerException e){
+                System.out.println(e);
+                data = 1;
+                System.out.println(x);
+                System.out.println("write default valie to a file");
+            }
 
         }catch(ArithmeticException e){
             System.out.println(e);
             data = 1;
-        }catch(NullPointerException e){
-            System.out.println(e);
-            data = 1;
-            System.out.println(x);
-            System.out.println("write default valie to a file");
         }finally {
             System.out.println("Run always no matter exception occures or not ... ");
             System.out.println("close the file ");
