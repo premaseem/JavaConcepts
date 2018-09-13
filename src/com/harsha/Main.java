81efc42a4371b9d8077073fbfd7378935bb2f29a
premaseem <premaseem.webmaster@gmail.com>
2018-09-13 12:19:32 -0500

    refactored

--- src/com/harsha/Main.java
+++
@@ -1,9 +0,0 @@
-package com.harsha;
-
-
-public class Main {
-
-    public static void main(String[] args) {
-        System.out.println("Harsha");
-    }
-}
--- src/com/mohan_vehicle/Vehicle.java
+++
@@ -1,9 +0,0 @@
-package com.mohan_vehicle;
-
-public interface Vehicle {
-	public String getVehicleType();
-	public String getVehicleName();
-	public Integer getNumberOfSeats();
-	public Integer getNumberOfWheels();
-
-}
